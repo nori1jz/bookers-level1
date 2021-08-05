70032c6a0838924560ede47997c2251aab6b0e57
nori1jz <apriotype2xxnorixx@gmail.com>
2021-08-05 16:38:27 +0000

    [add]新規投稿機能等

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,48 @@
 class BooksController < ApplicationController
   def top
-  end  
-  
+  end
+
   def index
+   @books = Book.all
+   
+
   end
 
   def show
+   @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
+
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+    redirect_to books_path
+
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.requre(:book).permit(:title, :body)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'top' => 'books#top'
-  
   resources :books
-  
+  get '/index' => 'books#index'
+  get 'show' => 'books#show'
+  post '/show' => 'books#show'
+  get 'books/show' 
+  get 'top' => 'books#top'
   root :to => 'books#top'
-
+  root :to => 'books#index'
+  patch 'books/:id' => 'books#update', as: 'update_book'
+  delete 'books/:id' => 'books#destroy', as: 'destroy_books'
 end
